77574f1b35bce55af51a56d8260b4f3a4bdfb7d4
ilmiraA <ilmiraa2007@gmail.com>
2023-08-19 15:01:07 +0300

    сделана регистрация

--- Script/vxod.js
+++
@@ -1,7 +0,0 @@
-$('body').on('click', '.password-checkbox', function(){
-	if ($(this).is(':checked')){
-		$('#password-input').attr('type', 'text');
-	} else {
-		$('#password-input').attr('type', 'password');
-	}
-}); 
